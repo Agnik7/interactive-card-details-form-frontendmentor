71a6424a12998e651eed147ab3221f74addaaa62
Agnik Bakshi <bakshi.agnik@gmail.com>
2024-01-05 10:09:40 +0530

    Back Card

---
+++ src/components/Card/Back.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Back() {
+  return (
+    <section className='w-[28rem] h-[15.3rem] bg-card-back mr-[1rem] flex items-center justify-end rounded-xl shadow-2xl'>
+        <h2 className='font-space mr-[3.5rem] font-light'>000</h2>
+    </section>
+  )
+}
--- src/components/Side.jsx
+++ src/components/Side.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Front from './Card/Front';
+import Back from './Card/Back';
 export default function Side() {
   return (
     <aside className='w-1/2 h-full flex flex-col justify-center items-end text-white'>
         <Front />
+        <Back />
     </aside>
   )
 }
